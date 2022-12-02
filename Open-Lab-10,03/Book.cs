4b04675c961da32ae09ff8dc41130ba990a98d80
maki <jantulamarko@outlook.com>
2022-12-02 17:49:39 +0100

    Add project files.

---
+++ Open-Lab-10,03/Book.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open_Lab_10._03
+{
+    internal class Book
+    {
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+        public string Title(string strtitle)
+        {
+            this.title = strtitle;
+            return strtitle;
+        }
+
+        public int Pages(int intpages)
+        {
+            this.pages = intpages;
+            return intpages;
+        }
+
+        public string Category(string strcategory)
+        {
+            this.category = strcategory;
+            return strcategory;
+        }
+
+        public string Author(string strauthor)
+        {
+            this.author = strauthor;
+            return strauthor;
+        }
+
+        public int ReleaseDate(int intreleaseDate)
+        {
+            this.releaseDate = intreleaseDate;
+            return intreleaseDate;
+        }
+
+        public void ans()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Title :        " + Title(title));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Pages :        " + Pages(pages));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Category :     " + Category(category));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Author :       " + Author(author));
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Release Date : " + ReleaseDate(releaseDate));
+            Console.WriteLine("----------------------------------------");
+        }
+
+    }
+}
---
+++ Open-Lab-10,03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Open_Lab_10._03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book LOTR = new Book();
+            LOTR.Title("The Lord of the Rings");
+            LOTR.Pages(9250);
+            LOTR.Category("epic high-fantasy novel");
+            LOTR.Author("John Ronald Reuel Tolkien");
+            LOTR.ReleaseDate(1954);
+
+            LOTR.ans();
+        }
+    }
+}
