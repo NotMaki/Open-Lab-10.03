97c8a35b43172edbb6cb63fd1659a2fad558a1ac
maki <jantulamarko@outlook.com>
2022-12-05 18:28:37 +0100

    Edited Program.cs

--- Open-Lab-10,03/Program.cs
+++ Open-Lab-10,03/Program.cs
@@ -8,11 +8,11 @@ namespace Open_Lab_10._03
         {
 
             Book LOTR = new Book();
-            LOTR.Title("The Lord of the Rings");
-            LOTR.Pages(9250);
-            LOTR.Category("epic high-fantasy novel");
-            LOTR.Author("John Ronald Reuel Tolkien");
-            LOTR.ReleaseDate(1954);
+            LOTR.Title = "The Lord of the Rings";
+            LOTR.Pages = 9250;
+            LOTR.Category = "epic high-fantasy novel";
+            LOTR.Author = "John Ronald Reuel Tolkien";
+            LOTR.ReleaseDate = 1954;
 
             LOTR.ans();
         }
